fd8f3536745ed98273995a29ed9411823240654d
Samuel Marvin <samjmarv@gmail.com>
2022-02-03 20:13:01 -0600

    black and red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+
+for (var i = 0; i < 81; i++){
+    let div = document.createElement('div');
+    div.style.width = '11.1%';
+    div.style.paddingBottom = '11.1%'
+    div.style.float = 'left';
+    if (i % 2 === 0){
+        div.style.backgroundColor = 'red';
+        document.body.appendChild(div);
+    } else {
+        div.style.backgroundColor = 'black';
+    }
+    document.body.appendChild(div)
+}
+
