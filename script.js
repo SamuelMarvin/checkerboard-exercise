8aa86e62f0cbe358bf4055afd8bc95838af96d73
Samuel Marvin <samjmarv@gmail.com>
2022-02-03 20:32:17 -0600

    random color checkerboard

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function randomColor()
+{
+    var Color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return Color;
+}
+
 
 for (var i = 0; i < 81; i++){
     let div = document.createElement('div');
@@ -5,10 +11,9 @@ for (var i = 0; i < 81; i++){
     div.style.paddingBottom = '11.1%'
     div.style.float = 'left';
     if (i % 2 === 0){
-        div.style.backgroundColor = 'red';
-        document.body.appendChild(div);
+        div.style.backgroundColor = randomColor();
     } else {
-        div.style.backgroundColor = 'black';
+        div.style.backgroundColor = randomColor();
     }
     document.body.appendChild(div)
 }
